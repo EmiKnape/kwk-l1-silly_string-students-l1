3e3b15e5e669a90ec68a1617450fdf628cc88fee
EmiKnape <enknape@aol.com>
2018-06-07 03:54:34 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -8,4 +8,4 @@
 def greeting(local_greeting, your_name, my_name, time_of_day)
   puts "#{local_greeting} #{your_name} I am #{my_name} how's your #{time_of_day}?"
 end
-greeting ("hey", "Luke", "Emi", "evening :)" )
+greeting("hey", "Luke", "Emi", "evening :)" )
