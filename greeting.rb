5a7a6756ad1de83c1d2c6fd96ac063e741c7f46b
EmiKnape <enknape@aol.com>
2018-06-07 03:50:21 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -3,3 +3,9 @@
 #end 
 #greeting("hey","Cindy", "Emi", "Night")
 #puts "Emi was here".reverse.upcase
+
+
+def greeting(local_greeting, your_name, my_name, time_of_day)
+  puts "#{local_greeting} #{your_name} I am #{my_name} how's your #{time_of_day}?"
+end
+greeting 
